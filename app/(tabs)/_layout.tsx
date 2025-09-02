f3dd1b937aaccc80b520c74912c5ccd1fc926ffe
dclinkenbeard <dclinkenbeard@csumb.edu>
2025-09-02 12:01:33 -0700

    Made a stub for the demo page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,6 +27,13 @@ export default function TabLayout() {
         }),
       }}>
       <Tabs.Screen
+        name="demo"
+        options={{
+          title: 'demo',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
+     <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
